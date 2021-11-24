72ae6824cac7e4c26c92fabe9bdc37687363e6c8
souhei <hanasouhei1211@gmail.com>
2021-11-24 19:10:42 +0900

    素因数分解関数を作成

--- typescript_app/src/index.ts
+++ typescript_app/src/index.ts
@@ -1,4 +1,29 @@
-window.addEventListener('load', (event) => {
-  let p = document.querySelector('#target')
-  p.textContent = 'TSへようこそ'
-})
+console.log("Node path = " + process.argv[0])
+console.log("script file path = " + process.argv[1])
+
+const data: number[] = []
+for (var i = 2; i < process.argv.length; i++) {
+  data.push(Number(process.argv[i]))
+}
+console.log(data)
+
+for (let item of data) {
+  const res = primeFactor(item)
+  console.log(item + '= ' + res)
+}
+
+function primeFactor(a: number): number[] {
+  const v: number[] = []
+  let x = a
+  let n = 2
+  while(x > n) {
+    if (x % n == 0) {
+      x = x / n
+      v.push(n)
+    } else {
+      n += n == 2 ? 1 : 2
+    }
+  }
+  v.push(x)
+  return v
+}
